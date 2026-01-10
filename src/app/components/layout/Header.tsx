1a1891ca0bb33feb8ad4c3ff41c1318c82c8427d
Sabyrka <sabyrkoo@gmail.com>
2026-01-10 21:10:44 +0500

    style: add hover and cursor interaction to Catalog button

--- src/app/components/layout/Header.tsx
+++ src/app/components/layout/Header.tsx
@@ -1,20 +1,27 @@
 import Image from 'next/image'
 import { LayoutGrid } from 'lucide-react'
+import Link from 'next/link'
 
 export const Header = () => {
   return (
-    <header className='flex'>
-      <Image
-        src="/logo.svg"
-        alt="Ozon"
-        width={140}
-        height={30}
-        priority
-      />
+    <header className="flex items-center">
+      <Link href="/">
+        <Image
+          src="/logo.svg"
+          alt="Ozon"
+          width={140}
+          height={30}
+          priority
+        />
+      </Link>
 
-      <button className='flex py-2.5 px-4 rounded-xl bg-primary text-white'>
-        <LayoutGrid className='mr-2' color="#fff" size={24} />
-        <span className='font-semibold'>Catalog</span>
+      <button className="flex py-2.5 px-4 rounded-xl bg-primary text-white cursor-pointer transition-colors hover:bg-primary/90">
+        <LayoutGrid
+          className="mr-2"
+          color="#fff"
+          size={24}
+        />
+        <span className="font-semibold">Catalog</span>
       </button>
     </header>
   )
