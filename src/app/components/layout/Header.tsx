58c05c287a564be1fa500300aa56faa58b276f3b
Sabyrka <sabyrkoo@gmail.com>
2026-01-10 23:56:44 +0500

    style: reorder classes and add cursor pointer to Everywhere button

--- src/app/components/layout/Header.tsx
+++ src/app/components/layout/Header.tsx
@@ -29,8 +29,8 @@ export const Header = () => {
       <div className="flex grow items-center rounded-xl bg-primary">
         <div className="flex grow items-center gap-2 rounded-xl bg-white m-0.5">
           <button
-            className="flex items-center gap-0.5 text-gray-500 text-sm p-1.5 m-1 bg-gray-100 rounded-xl transition-colors hover:text-primary cursor-pointer"
             title="Everywhere"
+            className="flex items-center gap-0.5 text-gray-500 text-sm p-1.5 m-1 bg-gray-100 rounded-xl transition-colors hover:text-primary cursor-pointer"
           >
             Everywhere
             <ChevronDown size={12} />
