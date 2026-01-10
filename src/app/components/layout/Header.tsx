f85183f3a15a29d2e4905f1b2984d2d43e8b1d00
Sabyrka <sabyrkoo@gmail.com>
2026-01-10 21:02:12 +0500

    feat: add Catalog button with icon to Header

--- src/app/components/layout/Header.tsx
+++ src/app/components/layout/Header.tsx
@@ -1,15 +1,21 @@
 import Image from 'next/image'
+import { LayoutGrid } from 'lucide-react'
 
 export const Header = () => {
   return (
-    <header>
+    <header className='flex'>
       <Image
-        src='/logo.svg'
-        alt='Ozon'
+        src="/logo.svg"
+        alt="Ozon"
         width={140}
         height={30}
         priority
       />
+
+      <button className='flex py-2.5 px-4 rounded-xl bg-primary text-white'>
+        <LayoutGrid className='mr-2' color="#fff" size={24} />
+        <span className='font-semibold'>Catalog</span>
+      </button>
     </header>
   )
 }
