319fa96fafd28326aa585761a6b2cbaf338b02c3
Sabyrka <sabyrkoo@gmail.com>
2026-01-11 06:04:11 +0500

    feat: add new page routes to PagesConfig

--- src/app/config/pages.config.ts
+++ src/app/config/pages.config.ts
@@ -5,4 +5,12 @@ export class PagesConfig {
   static readonly ORDERS = '/orders'
   static readonly FAVORITES = '/favorites'
   static readonly CART = '/cart'
+  
+  static readonly INSTALLMENT = '/installment'
+  static readonly KAZAKH_SELLERS = '/kazash-sellers'
+  static readonly CLOTHING = '/clothing'
+  static readonly ELECTRONICS = '/electronics'
+  static readonly HOUSE_AND_GARDEN = '/house-and-garden'
+  static readonly KIDS_PRODUCTS = '/kids-products'
+  static readonly HOUSEHOLD_APPLIANCES = '/household-appliances'
 }
